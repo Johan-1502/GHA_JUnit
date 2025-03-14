8d74103376ed632fba8a8d44d59dc00019b33b43
JohanGil1502 <johan.gil@uptc.edu.co>
2025-03-14 17:30:01 -0500

    feat: Inicio de la apliación

---
+++ src/test/java/com/gha_junit/service/CalculatorServiceTest.java
@@ -0,0 +1,19 @@
+package com.gha_junit.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+public class CalculatorServiceTest {
+
+    @Autowired
+    private CalculatorService calculatorService;
+
+    @Test
+    public void testAddition() {
+        int result = calculatorService.add(2, 3);
+        assertEquals(5, result);
+    }
+}
